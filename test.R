b97e2184f19967b5374030c8296c5ea60f10ae17
Cathy Zhang <jiezhang0116@gmail.com>
2022-03-27 21:53:48 +1100

    Initial commit

---
+++ test.R
@@ -0,0 +1,29 @@
+a <- array(c('green','yellow'),dim = c(3,3,2))
+print(a)
+
+# Create a vector.
+apple_colors <- c('green','green','yellow','red','red','red','green')
+
+# Create a factor object.
+factor_apple <- factor(apple_colors)
+
+# Print the factor.
+print(factor_apple)
+print(nlevels(factor_apple))
+
+# Create the data frame.
+BMI <- 	data.frame(
+  gender = c("Male", "Male","Female"), 
+  height = c(152, 171.5, 165), 
+  weight = c(81,93, 78),
+  Age = c(42,38,26)
+)
+print(BMI)
+
+print(class(BMI$Age))
+
+install.packages("tidyverse")
+library(tidyverse)
+
+glimpse(BMI)
+str(BMI)
